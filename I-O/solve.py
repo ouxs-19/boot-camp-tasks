8db59c09fa51e07975d10e54036e54fb948772c0
dark walker <dark-walker@ouxs.Shellmates>
2021-07-07 22:22:46 +0000

    third challenge

--- I-O/solve.py
+++ I-O/solve.py
@@ -1,12 +1,37 @@
 
 #1
-evens = [num for num in range(1,300) if num % 2 == 1]
+with open("student_names.txt","w") as f :
+	content = f.read()
+	f.write("Random but not so random")
 
 #2
-
-for num in evens :
-	print(len(evens),num**2)
+content = ""
+with open("student_names.txt") as f :
+	n = 10 
+	for i in range(n):
+		content += f.readline()
 
 
 #3
-print(57 in evens)
+
+content = ""
+with open("student_names.txt") as f :
+	n = 10 
+	for line in (file.readlines()[-n:]):
+		content += line
+
+
+#4
+
+with open("student_names.txt") as f :
+	x = "word"
+	print(x in f.read())
+
+
+#5
+
+chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+for char in chars:
+	with open(char+".txt","w") as f :
+		pass
