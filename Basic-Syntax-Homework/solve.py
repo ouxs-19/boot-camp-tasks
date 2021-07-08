50f48058c9ae3eb183a7398c44e95760d719f117
dark walker <dark-walker@ouxs.Shellmates>
2021-07-08 11:28:10 +0000

    added first question

--- Basic-Syntax-Homework/solve.py
+++ Basic-Syntax-Homework/solve.py
@@ -1,1 +1,42 @@
-evens = [num for num in range(1,300) if num % 2 == 0]
+
+#1
+
+with open("student_names.txt") as f :
+	content = f.read()
+
+
+#2
+with open("student_names.txt","w") as f :
+	f.write("Random but not so random")
+
+#3
+content = ""
+with open("student_names.txt") as f :
+	n = 10 
+	for i in range(n):
+		content += f.readline()
+
+
+#4
+
+content = ""
+with open("student_names.txt") as f :
+	n = 10 
+	for line in (file.readlines()[-n:]):
+		content += line
+
+
+#5
+
+with open("student_names.txt") as f :
+	x = "word"
+	print(x in f.read())
+
+
+#6
+
+chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+for char in chars:
+	with open(char+".txt","w") as f :
+		pass
